b17ee711ab78cbb4be49b39c998a35235ddb20d7
Sabbir Hossain <sabbir2always@gmail.com>
2025-06-04 20:01:31 +0600

    add copyright message

--- app/components/contacts.tsx
+++ app/components/contacts.tsx
@@ -5,58 +5,66 @@ import ContactForm from "./contact-form";
 
 export default function Contacts() {
   return (
-    <SectionWrapper
-      title="Contacts"
-      subTitle="Let's Get In Touch"
-      icon={Contact}
-    >
-      <div className=" grid grid-cols-1 gap-6 lg:grid-cols-2 ">
-        <div className=" flex items-center gap-4 group p-4 md:p-6 w-full rounded-lg border border-offWhite bg-cardGradient">
-          <div className="flex items-center gap-4">
-            <PhoneCall className="md:w-8 md:h-8 w-7 h-7 text-secondery" />
+    <footer>
+      <SectionWrapper
+        title="Contacts"
+        subTitle="Let's Get In Touch"
+        icon={Contact}
+        className="!pb-0"
+      >
+        <div className=" grid grid-cols-1 gap-6 lg:grid-cols-2 ">
+          <div className=" flex items-center gap-4 group p-4 md:p-6 w-full rounded-lg border border-offWhite bg-cardGradient">
+            <div className="flex items-center gap-4">
+              <PhoneCall className="md:w-8 md:h-8 w-7 h-7 text-secondery" />
+            </div>
+            <div>
+              <p className="md:text-lg text-base">Phone</p>
+              <p className=" md:text-xl text-lg font-medium">8801902317010</p>
+            </div>
+            <div className="ml-auto hidden md:block opacity-0 group-hover:opacity-100 duration-200">
+              <CopyButton copyData="+8801902317010" />
+            </div>
           </div>
-          <div>
-            <p className="md:text-lg text-base">Phone</p>
-            <p className=" md:text-xl text-lg font-medium">8801902317010</p>
-          </div>
-          <div className="ml-auto hidden md:block opacity-0 group-hover:opacity-100 duration-200">
-            <CopyButton copyData="+8801902317010" />
-          </div>
-        </div>
 
-        <div className=" flex items-center group gap-4 p-4 md:p-6 w-full rounded-lg border border-offWhite bg-cardGradient">
-          <div className="flex items-center gap-4">
-            <Mail className="md:w-8 md:h-8 w-7 h-7 text-secondery" />
+          <div className=" flex items-center group gap-4 p-4 md:p-6 w-full rounded-lg border border-offWhite bg-cardGradient">
+            <div className="flex items-center gap-4">
+              <Mail className="md:w-8 md:h-8 w-7 h-7 text-secondery" />
+            </div>
+            <div>
+              <p className="md:text-lg text-base">Email</p>
+              <p className=" md:text-xl text-lg font-medium">
+                sabbir2always@gmail.com
+              </p>
+            </div>
+            <div className="ml-auto hidden md:block opacity-0 group-hover:opacity-100 duration-200">
+              <CopyButton copyData="sabbir2always@gmail.com" />
+            </div>
           </div>
-          <div>
-            <p className="md:text-lg text-base">Email</p>
-            <p className=" md:text-xl text-lg font-medium">
-              sabbir2always@gmail.com
-            </p>
-          </div>
-          <div className="ml-auto hidden md:block opacity-0 group-hover:opacity-100 duration-200">
-            <CopyButton copyData="sabbir2always@gmail.com" />
-          </div>
-        </div>
 
-        <div className=" flex items-center group gap-4 p-4 md:p-6 w-full lg:col-span-2 rounded-lg border border-offWhite bg-cardGradient">
-          <div className="flex items-center gap-4">
-            <Map className="md:w-8 md:h-8 w-7 h-7 text-secondery" />
-          </div>
-          <div>
-            <p className="md:text-lg text-base">Address</p>
-            <p className=" md:text-xl text-lg font-medium">
-              Pabna, Rajshahi, Bangladesh
-            </p>
-          </div>
-          <div className="ml-auto hidden md:block opacity-0 group-hover:opacity-100 duration-200">
-            <CopyButton copyData="Pabna, Rajshahi, Bangladesh" />
+          <div className=" flex items-center group gap-4 p-4 md:p-6 w-full lg:col-span-2 rounded-lg border border-offWhite bg-cardGradient">
+            <div className="flex items-center gap-4">
+              <Map className="md:w-8 md:h-8 w-7 h-7 text-secondery" />
+            </div>
+            <div>
+              <p className="md:text-lg text-base">Address</p>
+              <p className=" md:text-xl text-lg font-medium">
+                Pabna, Rajshahi, Bangladesh
+              </p>
+            </div>
+            <div className="ml-auto hidden md:block opacity-0 group-hover:opacity-100 duration-200">
+              <CopyButton copyData="Pabna, Rajshahi, Bangladesh" />
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <ContactForm />
-      </div>
-    </SectionWrapper>
+        <div>
+          <ContactForm />
+        </div>
+        <div className="py-10">
+          <p className="text-center text-sm">
+            &copy; 2025 Sabbir Hossain, All rights reserved
+          </p>
+        </div>
+      </SectionWrapper>
+    </footer>
   );
 }
